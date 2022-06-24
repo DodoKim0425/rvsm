0f007296ab885a2c173735434b1406404a11fb50
DoYeon <dodoyeon0425@gmail.com>
2022-06-24 23:35:40 +0900

    서버 코드 변경에 따른 그래프 페이지 코드 수정, 차트화면과 메인화면 xml수정, themes.xml 수정

--- app/src/main/java/com/example/rvsm/ChartActivity.java
+++ app/src/main/java/com/example/rvsm/ChartActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -51,8 +52,14 @@ public class ChartActivity extends AppCompatActivity {
     private ImageButton chart_back_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_chart);
+
+
+
         chart_back_btn=findViewById(R.id.chart_back_btn);
         tv_admin=findViewById(R.id.tv_admin);
         tv_patient_name=findViewById(R.id.tv_patient_name);
@@ -67,8 +74,8 @@ public class ChartActivity extends AppCompatActivity {
         Date date=new Date(cal.getTimeInMillis());
         SimpleDateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String getTime=dateFormat.format(date);
-        //last_time=getTime;//최초 연결시 현재 시간 기준으로 5초 전 데이터까지 가져온다 실제 테스트시 이 부분 사용함
-        last_time="2022-05-20 17:30:26";//aws 서버 db연결 테스트를 위한 임시코드입니다
+        last_time=getTime;//최초 연결시 현재 시간 기준으로 5초 전 데이터까지 가져온다 실제 테스트시 이 부분 사용함
+        //last_time="2022-06-11 12:30:26";//aws 서버 db연결 테스트를 위한 임시코드입니다
         System.out.println("현재시간-5: "+last_time);
         chart_back_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,13 +92,13 @@ public class ChartActivity extends AppCompatActivity {
         System.out.println("연결함수에 들어옴");
         try {
             System.out.println("연결시도");
-            mSocket = IO.socket("http://13.125.227.19:3005");//서버 주소, 포트는 3005, 서버코드 합친 후 포트변경필요
+            mSocket = IO.socket("http://13.125.227.19:3000");//서버 주소, 포트는 3005, 서버코드 합친 후 포트변경필요
             //mSocket=IO.socket("http://10.0.2.2:3005");//로컬호스트에서 테스팅시 사용
             mSocket.connect();
             System.out.println("연결성공");
             Log.d("SOCKET", "Connection success : " + mSocket.id());
 
-            mSocket.on("get_data", new Emitter.Listener() {//데이터 받는 이벤트
+            mSocket.on("response_data", new Emitter.Listener() {//데이터 받는 이벤트
                 @Override
                 public void call(Object... args) {
                     try{
@@ -144,7 +151,7 @@ public class ChartActivity extends AppCompatActivity {
                     }
                 }
             });
-            mSocket.emit("and_request_user_info",patient_id);
+            mSocket.emit("web_request_user_info",patient_id);
         } catch (Exception e) {
             System.out.println("연결실패");
             Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG)
@@ -158,29 +165,32 @@ public class ChartActivity extends AppCompatActivity {
         System.out.println("보낼시간: "+last_time);
         try {
             data.put("time",last_time);
-            data.put("patient_id","PATIENT_STATE");
+            data.put("user_name","PATIENT_STATE");
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        mSocket.emit("and_connected",data);//서버에 데이터 요청
+        mSocket.emit("request_data",data);//서버에 데이터 요청
 
     }
     void doJSONParser(JSONArray index){//서버로 부터 받은 jsonobject에서 파싱
 
         try{
-            last_time=index.getJSONObject(index.length()-1).getString("TIME");
-            for (int i = 0; i < index.length(); i++) {
-                JSONObject jsonObject_data = index.getJSONObject(i);
-                entryList1.add(new Entry(time,Integer.parseInt(jsonObject_data.getString("HeartRate"))));
-                entryList2.add(new Entry(time,Float.parseFloat(jsonObject_data.getString("Temperature"))));
-                entryList3.add(new Entry(time,Integer.parseInt(jsonObject_data.getString("Spo2"))));
-                time++;
-                String[]arr=jsonObject_data.getString("TIME").split(" ");
-                xVals.add(arr[1]);
+
+                last_time=index.getJSONObject(index.length()-1).getString("TIME");
+                for (int i = 0; i < index.length(); i++) {
+                    JSONObject jsonObject_data = index.getJSONObject(i);
+                    entryList1.add(new Entry(time,Integer.parseInt(jsonObject_data.getString("HeartRate"))));
+                    entryList2.add(new Entry(time,Float.parseFloat(jsonObject_data.getString("Temperature"))));
+                    entryList3.add(new Entry(time,Integer.parseInt(jsonObject_data.getString("Spo2"))));
+                    time++;
+                    String[]arr=jsonObject_data.getString("TIME").split(" ");
+                    xVals.add(arr[1]);
                 /*if(i==(index.length()-1)){
                     last_time=jsonObject_data.getString("TIME");
                 }*/
-            }
+                }
+
+
             drawGraph();//그래프 그리기
         }
         catch (JSONException e){ System.out.println("파서에러");}
@@ -189,38 +199,6 @@ public class ChartActivity extends AppCompatActivity {
     }
     private void drawGraph(){//그래프를 그린다
         try{
-            dataSet1=new LineDataSet(entryList1,"HeartRate");//선모양을 정해주는 데이터를 넣음
-
-            dataSet2=new LineDataSet(entryList2,"Temperature");
-
-            dataSet3=new LineDataSet(entryList3,"Spo2");
-
-            //dataset1선 모양을 정함
-            dataSet1.setDrawValues(false);
-            dataSet1.setColor(Color.parseColor("#F79AB3"));
-            dataSet1.setDrawCircles(false);
-            dataSet1.setLineWidth(3);
-
-            //dataset2선 모양 정함
-            dataSet2.setDrawValues(false);
-            dataSet2.setColor(Color.parseColor("#F7C69A"));
-            dataSet2.setDrawCircles(false);
-            dataSet2.setLineWidth(3);
-
-            //dataset3선 모양 정함
-            dataSet3.setDrawValues(false);
-            dataSet3.setColor(Color.parseColor("#9560FF"));
-            dataSet3.setDrawCircles(false);
-            dataSet3.setLineWidth(3);
-
-            LineData chartData=new LineData();//선들여러개를 묶는객체임
-            chartData.addDataSet(dataSet1);
-            chartData.addDataSet(dataSet2);
-            chartData.addDataSet(dataSet3);
-
-            lineChart.setData(chartData);//그래프위에 선들을 나타냄
-
-            lineChart.setVisibleXRangeMaximum(20);//한 그래프위에 20개의 데이터만 보이도록함
             XAxis xAxis=lineChart.getXAxis();
             xAxis.setDrawLabels(true);
             xAxis.setPosition(XAxis.XAxisPosition.TOP);//x레이블 위치
@@ -231,6 +209,43 @@ public class ChartActivity extends AppCompatActivity {
             xAxis.setGranularity(1f);
 
             xAxis.setLabelRotationAngle(315f);//x레이블 기울기
+
+            YAxis yAxis=lineChart.getAxisLeft();
+            yAxis.setAxisMinimum(0);
+            yAxis.setAxisMaximum(110);
+            dataSet1=new LineDataSet(entryList1,"HeartRate");//선모양을 정해주는 데이터를 넣음
+
+            //dataSet2=new LineDataSet(entryList2,"Temperature");
+
+            dataSet3=new LineDataSet(entryList3,"Spo2");
+
+            //dataset1선 모양을 정함
+            dataSet1.setDrawValues(false);
+            dataSet1.setColor(Color.parseColor("#ff6384"));
+            dataSet1.setDrawCircles(false);
+            dataSet1.setLineWidth(3);
+
+            //dataset2선 모양 정함
+            //dataSet2.setDrawValues(false);
+           // dataSet2.setColor(Color.parseColor("#F7C69A"));
+           // dataSet2.setDrawCircles(false);
+            //dataSet2.setLineWidth(3);
+
+           // dataset3선 모양 정함
+            dataSet3.setDrawValues(false);
+            dataSet3.setColor(Color.parseColor("#5762ff"));
+            dataSet3.setDrawCircles(false);
+            dataSet3.setLineWidth(3);
+
+            LineData chartData=new LineData();//선들여러개를 묶는객체임
+            chartData.addDataSet(dataSet1);
+           // chartData.addDataSet(dataSet2);
+            chartData.addDataSet(dataSet3);
+
+            lineChart.setData(chartData);//그래프위에 선들을 나타냄
+
+            lineChart.setVisibleXRangeMaximum(6);//한 그래프위에 6개의 데이터만 보이도록함
+
             lineChart.moveViewToX(dataSet1.getEntryCount());//x축을 가장 최근값이 보이도록 옮김, 가장 최근의 20개만 보이게됨
 
         }catch (Exception e){
